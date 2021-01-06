ca5f89e58924b59a7a7d7101e61c92efdebe0775
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2021-01-06 23:08:49 +0100

    feat(main): reduce

--- src/arrayF.ts
+++ src/arrayF.ts
@@ -3,12 +3,6 @@ import { tail } from './array';
 import { head } from './array_to_item';
 
 type FnReduce = (acc: any) => (f: FnGeneric2<any>) => (arr: any[]) => any;
-export const reduce: FnReduce = (acc) => (f) => (arr) => {
-  if (arr.length > 0) {
-    const first = head(arr);
-    const rest = tail(arr);
-    const newAcc = f(acc)(first);
-    return reduce(newAcc)(f)(rest);
-  }
-  return acc;
-};
+export const reduce: FnReduce = (acc) => (f) => (arr) => (arr.length > 0
+  ? reduce(f(acc)(head(arr)))(f)(tail(arr))
+  : acc);
